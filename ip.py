83f91ef5d494c7fe20b855c90c626e1c2ae3471f
Pourquoiiiiiiiii <132891555+Pourquoiiiiiiiii@users.noreply.github.com>
2024-06-23 02:19:14 -0700

    Update ip.py

--- ip.py
+++ ip.py
@@ -1,16 +1,16 @@
 import re
 
-def mi_sa_find_ton_momon(fbr_tonzob: str):
-    out_mareine = re.compile(r'(\d{1,3}\.){3}\d{1,3}')
+def t(__: str):
+    ___ = re.compile(r'(\d{1,3}\.){3}\d{1,3}')
     
-    with open(fbr_tonzob, 'r', encoding='utf-8') as file:
-        lamoukate = file.readlines()
+    with open(__, 'r', encoding='utf-8') as file:
+        _ = file.readlines()
         
-        for bour_tonser, tser in enumerate(lamoukate, start=1):
-            out_papa = out_mareine.search(tser)
-            if out_papa:
-                print(f"Ligne {bour_tonser}: {tser.strip()} (IP: {out_papa.group(0)})")
+        for _____, tser in enumerate(_, start=1):
+            _______ = ___.search(tser)
+            if _______:
+                print(f"Ligne {_____}: {tser.strip()} (IP: {_______.group(0)})")
 
 if __name__ == "__main__":
-    fbr_tonzob = 'tfd.txt'
-    mi_sa_find_ton_momon(fbr_tonzob)
+    __ = 'tfd.txt'
+    t(__)
